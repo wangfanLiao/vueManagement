90c2a43dee928c83082747c8c3dd4c6dc6224040
Gazzila W <793901853@qq.com>
2024-05-24 15:27:21 +0800

    feat: 增加login表单验证，大部分代码见element\&element-plusform校验。

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,50 +1,95 @@
 <template>
-  <div id="app">
-    <div class="login-container">
-      <el-form ref="formRef" :model="form" class="login-form">
-        <div class="title-container">
-          <h3 class="title">用户登录</h3>
-        </div>
-        <el-form-item>
-          <el-icon class="is-loading">
-            <Loading />
-          </el-icon>
-          <el-input v-model="form.name" />
-        </el-form-item>
-        <el-form-item>
-          <!-- <el-icon class="is-loading">
+  <div class="login-container">
+    <el-form
+      ref="formRef"
+      :model="form"
+      :rules="rules"
+      class="login-form"
+      label-width="auto"
+      style="max-width: 600px"
+    >
+      <div class="title-container">
+        <h3 class="title">
+          用户登录<el-icon><SwitchFilled /></el-icon>
+        </h3>
+      </div>
+      <el-form-item label="UserName" prop="username" class="elFormItem">
+        <el-input v-model="form.username">
+          <template #prepend
+            ><el-icon class="is-loading"> <Loading /> </el-icon
+          ></template>
+        </el-input>
+      </el-form-item>
+      <el-form-item label="password" prop="password">
+        <!-- <el-icon class="is-loading">
             <Loading />
           </el-icon> -->
-          <svg-icon icon="password"></svg-icon>
-          <el-input v-model="form.password" />
-        </el-form-item>
-        <el-button type="danger" plain @click="loginVerify">Login</el-button>
-      </el-form>
-    </div>
+
+        <el-input
+          v-model="form.password"
+          type="password"
+          placeholder="Please input password"
+          show-password
+          ><template #prepend><svg-icon icon="password"></svg-icon></template
+        ></el-input>
+      </el-form-item>
+
+      <el-button type="danger" plain @click="handleLogin">Login</el-button>
+    </el-form>
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue'
-import { Loading } from '@element-plus/icons-vue'
-import { mapState } from 'vuex'
-import store from '@/store/index'
-import user from '@/store/date.js'
+import { ref, reactive } from 'vue'
+import { Loading, SwitchFilled } from '@element-plus/icons-vue'
 
-console.log(user.user)
-const formRef = ref()
+const formRef = ref(null)
 const form = ref({
-  name: 'admin',
-  password: '123456'
+  username: '',
+  password: ''
 })
-const state = mapState({
-  getState: (state) => console.log(state)
+const rules = reactive({
+  username: [
+    {
+      required: true,
+      message: 'Please input Activity username',
+      trigger: 'blur'
+    },
+    { min: 3, max: 20, message: 'Length should be 3 to 20', trigger: 'blur' }
+  ],
+  password: [
+    {
+      required: true,
+      message: 'Please input Activity password',
+      trigger: 'blur'
+    },
+    { min: 3, max: 20, message: 'Length should be 3 to 20', trigger: 'blur' }
+  ]
 })
-function loginVerify() {
-  console.log(state.getState)
-  console.log(store)
-  console.log(this.$store)
+
+const handleLogin = () => {
+  formRef.value.validate((valid) => {
+    if (valid) {
+      alert('submit!')
+    } else {
+      console.log('error submit!!')
+      return false
+    }
+  })
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.title-container {
+  position: relative;
+  width: 120px;
+  left: 0px;
+  top: 0px;
+  border: 0px solid #e90a4d;
+}
+.elFormItem {
+  position: relative;
+  top: 5px;
+  border: 0px solid #e90a4d;
+}
+</style>
